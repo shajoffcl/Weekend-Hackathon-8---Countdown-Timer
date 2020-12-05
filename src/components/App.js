c48303f5e2c6b7f2e8fa2ac812a1b08d69240b7a
Mohd Shaib <shaj.native@gmail.com>
2020-12-05 17:14:30 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,48 @@
-import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
-
+  const [counter, setCounter] = useState("");
+  const [controller, setController] = useState(false);
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      const value = event.target.value;
+      if (isNaN(value)) {
+        setCounter(0);
+        return;
+      }
+      setCounter(Math.floor(value));
+      setController(true);
+    }
+  };
+  const handleInterval = () => {
+    if (!controller || counter === 0) {
+      return;
+    }
+    let newCounter = counter - 1;
+    setCounter(newCounter);
+  };
+  useEffect(() => {
+    const id = setInterval(handleInterval, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  });
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input
+            id="timeCount"
+            onKeyDown={(event) => handleKeyDown(event)}
+          />{" "}
+          sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{counter}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
